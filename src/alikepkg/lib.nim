b82b284041dba2920ba5a444512319c8907ab52f
crystal <crystal@tsunde.ru>
2022-10-11 11:54:43 +0300

    implement RGBA hash

--- src/alikepkg/lib.nim
+++ src/alikepkg/lib.nim
@@ -14,6 +14,9 @@ type
     PixelArray* = array[HASH_SIZE * 8, uint8]
     HashArray* = array[HASH_SIZE, uint8]
 
+    RGBAArray* = array[HASH_SIZE * 8, ColorRGBA]
+    RGBAHash* = tuple[r, g, b, a: HashArray]
+
 
 const SMALL_IMG_SIDE = sqrt((HASH_SIZE * 8).float64).int
 when SMALL_IMG_SIDE <= 1:
@@ -30,45 +33,87 @@ iterator iterPixelValues(img: Image): uint8 =
     ## Iterate over average of rgb channels pixel values
     ## Gray-scale pixels basically
     for col in img.data:
-        let channel_sum = col.r.uint16 + col.g.uint16 + col.g.uint16
-        yield channel_sum.floorDiv(3).uint8
+        let channelSum = col.r.uint16 + col.g.uint16 + col.g.uint16
+        yield channelSum.floorDiv(3).uint8
+
+iterator iterPixelColors(img: Image): ColorRGBA =
+    ## Iterate over RGBA colors of pixels in the image
+    for col in img.data:
+        yield col
 
 
 proc getSimpleImgHash*(img: Image): HashArray =
-    ## Compute simple perceptive hash for image
+    ## Compute simple perceptual hash for image
 
-    let small_img = img.getSmallImg
+    let smallImg = img.getSmallImg
 
-    var pixel_values: PixelArray
-    for i, pv in enumerate(small_img.iterPixelValues):
-        pixel_values[i] = pv
+    var pixelValues: PixelArray
+    for i, pv in enumerate(smallImg.iterPixelValues):
+        pixelValues[i] = pv
 
     let avg = block:
-        var fv = newSeq[float](pixel_values.len)
-        for i in 0..pixel_values.high:
-            fv[i] = pixel_values[i].float
         var rs: RunningStat
-        rs.push(fv)
+        for i in 0..pixelValues.high:
+            rs.push(pixelValues[i].int)
         rs.mean.uint8
 
-    for i, pv in enumerate(pixel_values):
+    for i, pv in enumerate(pixelValues):
         let idx = floorDiv(i, 8)
         let shift = i mod 8
-        if pv > avg.uint8:
+        if pv > avg:
             result[idx] = result[idx] or (1'u8 shl shift)
+
+proc getRGBAImgHash*(img: Image): RGBAHash =
+    ## Compute RGBAHash perceptual of an image
+    
+    let smallImg = img.getSmallImg
+
+    var pixelRGBAs: RGBAArray
+    for i, pix in enumerate(smallImg.iterPixelColors):
+        pixelRGBAs[i] = pix
+    
+    let (rAvg, gAvg, bAvg, aAvg) = block:
+        var rRs : RunningStat
+        var gRs : RunningStat
+        var bRs : RunningStat
+        var aRs : RunningStat
+        for i in 0..pixelRGBAs.high:
+            rRs.push(pixelRGBAs[i].r.int)
+            gRs.push(pixelRGBAs[i].g.int)
+            bRs.push(pixelRGBAs[i].b.int)
+            aRs.push(pixelRGBAs[i].a.int)
+        (rRs.mean.uint8,
+         gRs.mean.uint8,
+         bRs.mean.uint8,
+         aRs.mean.uint8)
+    
+    template cmpBitMask(val, threshold, arr, idx, shift: untyped): untyped =
+        if val > threshold:
+            arr[idx] = arr[idx] or (1'u8 shl shift)
+
+    for i, pix in enumerate(pixelRGBAs):
+        let idx = floorDiv(i, 8)
+        let shift = i mod 8
+        
+        cmpBitMask(pix.r, rAvg, result.r, idx, shift)
+        cmpBitMask(pix.g, gAvg, result.g, idx, shift)
+        cmpBitMask(pix.b, bAvg, result.b, idx, shift)
+        cmpBitMask(pix.a, aAvg, result.a, idx, shift)
+        
+
 
 proc hexDigest*(hash: HashArray): string =
     ## Return hex digest of an image hash
     result = block:
-        var hex_reprs = newSeq[string](HASH_SIZE)
+        var hexReprs = newSeq[string](HASH_SIZE)
         for i, v in enumerate(hash):
-            hex_reprs[i] = v.toHex
-        hex_reprs.join("")
+            hexReprs[i] = v.toHex
+        hexReprs.join("")
 
 proc diff*(hash1, hash2: HashArray): float64 =
     ## Compute diff for an image
-    let total_bits = HASH_SIZE * 8
-    var diff_bits = 0
+    let totalBits = HASH_SIZE * 8
+    var diffBits = 0
     for (v1, v2) in zip(hash1, hash2):
         var d = v1 xor v2
         if d == 0: continue
@@ -76,4 +121,14 @@ proc diff*(hash1, hash2: HashArray): float64 =
         while d > 0:
             diff_bits = diff_bits + (d and 1).int
             d = d shr 1
-    result = diff_bits / total_bits
+    result = diff_bits / totalBits
+
+proc diff*(hash1, hash2: RGBAHash): float64 =
+    ## Compute diff for an image using RGBAHash
+    
+    return (
+        hash1.r.diff(hash2.r) +
+        hash1.g.diff(hash2.g) +
+        hash1.b.diff(hash2.b) +
+        hash1.a.diff(hash2.a)
+    ) / 4
