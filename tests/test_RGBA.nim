dfea32bfd5ccd566e8b389033430a7b5e60d91fe
crystal <crystal@tsunde.ru>
2022-10-11 12:01:37 +0300

    More precise test description in RGBAHash tests

--- tests/test_RGBA.nim
+++ tests/test_RGBA.nim
@@ -11,14 +11,14 @@ test "Test RGBAHash same image equality":
     
     check img1.getRGBAImgHash.diff(img2.getRGBAImgHash) == 0.0
 
-test "Test HUE rotated 45 diff < 0.1":
+test "Test RGBAHash HUE rotated 45 diff < 0.1":
     let
         img1 = readImage("tests/assets/testimg1.png")
         img2 = readImage("tests/assets/testimg1_hue_rotated_45.png")
     
     check img1.getRGBAImgHash.diff(img2.getRGBAImgHash) < 0.1
 
-test "Test desatureated image diff < 0.1":
+test "Test RGBAHash desatureated image diff < 0.1":
     let
         img1 = readImage("tests/assets/testimg1.png")
         img2 = readImage("tests/assets/testimg1_desaturated.png")
